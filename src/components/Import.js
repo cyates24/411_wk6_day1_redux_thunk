be9d70429a2e0e413910ea5042d16e3bf1c98f60
Connor Yates <connorwyates24@gmail.com>
2020-02-16 17:20:18 -0600

    final commit

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from "react";
+import {
+  Container,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Button
+} from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
 
-const Import = (props) => {
-    // fill out this component
+const Import = props => {
+  // fill out this component
 
-    return (
-        <p>Import Component</p>
-    )
-}
+  return (
+    <Container maxWidth="lg" className="car-container">
+      <Button onClick={props.fetchMakes} variant="contained" color="primary">
+        Import
+      </Button>
+      <Table>
+        <TableBody>
+          {props.makes.map((make, id) => (
+            <TableRow key={make.id}>
+              <TableCell component="th" scope="row">
+                {make.MakeId}
+              </TableCell>
+              <TableCell>{make.MakeName}</TableCell>
+              <TableCell>
+                <DeleteIcon
+                  onClick={() => props.deleteMake(id)}
+                  className="icon text-red"
+                />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Container>
+  );
+};
 
-export default Import
+export default Import;
--- src/containers/Import.js
+++ src/containers/Import.js
@@ -1,1 +1,20 @@
 // fill out this container
+import { connect } from 'react-redux'
+import Import from '../components/Import'
+import {fetchMakes, deleteMake} from '../redux/actions'
+
+
+const mapStateToProps = (state) => {
+    return {
+        makes: state.makes
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+      fetchMakes: (makes) => dispatch(fetchMakes(makes)),
+      deleteMake: (make) => dispatch(deleteMake(make))
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Import)
